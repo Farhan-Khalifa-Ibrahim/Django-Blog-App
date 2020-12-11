9156809e7b1f19d8a4b3ab26cde55a2109d901fa
Farhan-Khalifa-Ibrahim <farhankhalifa1510@gmail.com>
2020-12-11 17:10:11 +0800

    Adding unit test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+
+
+class BlogTests (TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@gmail.com',
+            password='secret'
+        )
+
+        self.post = Post.objects.create(
+            title='A good little boy',
+            body='testing body',
+            author=self.user
+        )
+
+    def test_string_reperesntation(self):
+        post = Post(title='A simple title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good little boy')
+        self.assertEqual(f'{self.post.body}', 'testing body')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'testing body')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good little boy')
+        self.assertTemplateUsed(response, 'post_detail.html')
